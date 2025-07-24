46937ae133acbebb0fc91284388ebb2279e4d97d
kap <lamse@naver.com>
2025-07-24 22:59:33 +0900

    git init

---
+++ src/main/java/hey/lpp/LppApplication.java
@@ -0,0 +1,13 @@
+package hey.lpp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hey/lpp/LppApplicationTests.java
@@ -0,0 +1,13 @@
+package hey.lpp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
