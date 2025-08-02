89d4aa9af6bfaecd3d6ec8a73f5515e705783845
kap <lamse@naver.com>
2025-08-02 22:57:10 +0900

    멀티 업로드 파일 발리데이션

--- src/main/java/hey/lpp/domain/product/ProductForm.java
+++ src/main/java/hey/lpp/domain/product/ProductForm.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.URL;
 import org.springframework.web.multipart.MultipartFile;
+import java.util.List;
 
 @Data
 @NoArgsConstructor
@@ -13,8 +14,8 @@ public class ProductForm {
     @NotEmpty
     private String name;
 
-    @ValidFile(allowFileDefines = {"jpeg", "png", "jpg"}, message = "이미지 파일은 필수입니다.")
-    private MultipartFile imageFile;
+    @ValidFile(allowFileDefines = {"jpeg", "png", "jpg"}, message = "이미지 파일을 1개 이상 선택해 주세요.")
+    private List<MultipartFile> imageFiles;
 
     @NotEmpty
     @URL
--- src/main/java/hey/lpp/domain/product/ValidFileValidator.java
+++ src/main/java/hey/lpp/domain/product/ValidFileValidator.java
@@ -1,19 +1,24 @@
 package hey.lpp.domain.product;
 
+import hey.lpp.util.FileStore;
 import hey.lpp.util.FilenameUtil;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.ObjectUtils;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 import org.thymeleaf.util.ArrayUtils;
 
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.List;
 
 @Slf4j
-public class ValidFileValidator implements ConstraintValidator<ValidFile, MultipartFile> {
-
+@RequiredArgsConstructor
+public class ValidFileValidator implements ConstraintValidator<ValidFile, List<MultipartFile>> {
+    private final FileStore fileStore;
     private ValidFile annotation;
 
     @Override
@@ -22,50 +27,48 @@ public class ValidFileValidator implements ConstraintValidator<ValidFile, Multip
     }
 
     @Override
-    public boolean isValid(MultipartFile multipartFile, ConstraintValidatorContext constraintValidatorContext) {
-
-        if (!annotation.required()) {
-            if (multipartFile.isEmpty()) {
-                return true;
-            }
-        }
-
-        if (multipartFile.isEmpty()) {
-            constraintValidatorContext.buildConstraintViolationWithTemplate("업로드 대상 파일이 없습니다.").addConstraintViolation();
-            return false;
-        }
-
-        final String fileName = multipartFile.getOriginalFilename();
-        if (!StringUtils.hasText(fileName)) {
-            constraintValidatorContext.buildConstraintViolationWithTemplate("업로드 요청한 파일명이 존재하지 않습니다.").addConstraintViolation();
-            return false;
-        }
-
-        try {
-            int targetByte = multipartFile.getBytes().length;
-            if (targetByte == 0) {
-                constraintValidatorContext.buildConstraintViolationWithTemplate("파일의 용량이 0 byte입니다.").addConstraintViolation();
+    public boolean isValid(List<MultipartFile> files, ConstraintValidatorContext context) {
+        boolean result = true;
+        for (MultipartFile file : files) {
+            if (file.isEmpty()) {
+                if (!annotation.required()) {
+                    return true;
+                }
+                //context.buildConstraintViolationWithTemplate("업로드 대상 파일이 없습니다.").addConstraintViolation();
                 return false;
             }
-        } catch (IOException e) {
-            log.error(e.getMessage(), e);
-            constraintValidatorContext.buildConstraintViolationWithTemplate("파일의 용량 확인 중 에러가 발생했습니다.").addConstraintViolation();
-            return false;
+
+            final String fileName = file.getOriginalFilename();
+//            if (!StringUtils.hasText(fileName)) {
+//                continue; // 파일명이 비어있으면 건너뜀
+//            }
+
+            try {
+                int targetByte = file.getBytes().length;
+                if (targetByte == 0) {
+                    context.buildConstraintViolationWithTemplate("파일(" + fileName + ")의 용량이 0 byte입니다.").addConstraintViolation();
+                    result = false;
+                    continue;
+                }
+            } catch (IOException e) {
+                log.error(e.getMessage(), e);
+                context.buildConstraintViolationWithTemplate("파일(" + fileName + ")의 용량 확인 중 에러가 발생했습니다.").addConstraintViolation();
+                result = false;
+                continue;
+            }
+
+            final String[] allowExtArray = annotation.allowFileDefines();
+            final String fileExt = FilenameUtil.getExtension(fileName);
+
+            //파일명의 허용 확장자 검사
+            if (!ArrayUtils.contains(allowExtArray, fileExt.toLowerCase())) {
+                String sb = "허용되지 않는 확장자(" + fileExt + ")의 파일이 포함되어 있습니다. 다음 확장자들만 허용됩니다." +
+                        " [" + Arrays.toString(allowExtArray)  + "]";
+                context.buildConstraintViolationWithTemplate(sb).addConstraintViolation();
+                result = false;
+            }
         }
 
-
-        final String[] allowExtArray = annotation.allowFileDefines();
-        final String fileExt = FilenameUtil.getExtension(fileName);
-
-        //파일명의 허용 확장자 검사
-        if (!ArrayUtils.contains(allowExtArray, fileExt.toLowerCase())) {
-            String sb = "허용되지 않는 확장자의 파일입니다. 다음 확장자들만 허용됩니다." +
-                    ": " + Arrays.toString(allowExtArray);
-            constraintValidatorContext.buildConstraintViolationWithTemplate(sb).addConstraintViolation();
-
-            return false;
-        }
-
-        return true;
+        return result;
     }
 }
