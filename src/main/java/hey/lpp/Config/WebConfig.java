8fc98984a3629ac0e653d873a057f74974250f04
kap <lamse@naver.com>
2025-08-10 22:17:10 +0900

    cors 설정 로컬 개발서버에서만 작동하도록 수정

--- src/main/java/hey/lpp/Config/WebConfig.java
+++ src/main/java/hey/lpp/Config/WebConfig.java
@@ -1,16 +1,18 @@
 package hey.lpp.Config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@Profile("dev") // This configuration will only be active in 'dev' profile
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("http://localhost:*", "http://127.0.0.1:*", "https://lamse.github.io:*")
+                .allowedOriginPatterns("http://localhost:*", "http://127.0.0.1:*")
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(true);
