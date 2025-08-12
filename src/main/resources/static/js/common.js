c74afbe49a83f086dea6f37796758b5abebabb3b
kap <lamse@naver.com>
2025-08-12 22:58:38 +0900

    기술 스택 수정

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -1,8 +1,17 @@
 $(function() {
-    $('#openTechStack').on('click', function() {
-        $('#techStackModal').removeClass('hidden');
+    $('#openTechStack').on('mouseover', function() {
+        $('#techStackModal').removeClass('opacity-0 pointer-events-none');
+        $('#techStackModal').addClass('opacity-100');
     });
     $('#closeTechStack').on('click', function() {
-        $('#techStackModal').addClass('hidden');
+        $('#techStackModal').removeClass('opacity-100');
+        $('#techStackModal').addClass('opacity-0 pointer-events-none');
+    });
+
+    $(document).on('click', function(event) {
+        if (!$(event.target).closest('#techStackModal .bg-white').length && !$(event.target).is('#openTechStack')) {
+            $('#techStackModal').removeClass('opacity-100');
+            $('#techStackModal').addClass('opacity-0 pointer-events-none');
+        }
     });
 });
